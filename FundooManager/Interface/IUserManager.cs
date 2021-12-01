c028887caac036cefb766c08c264ea8279ce1e99
mohdhanif1234 <hanif.topia@gmail.com>
2021-12-01 11:59:01 +0530

    [Hanif]Refactor: Implemented Stylecop for UserManager class and its interface

--- FundooManager/Interface/IUserManager.cs
+++ FundooManager/Interface/IUserManager.cs
@@ -1,13 +1,52 @@
-﻿using FundooModel;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IUserManager.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Mohammad Hanif"/>
+// ----------------------------------------------------------------------------------------------------------
 
 namespace FundooManager.Interface
 {
+    using FundooModel;
+
+    /// <summary>
+    /// This is an interface of the UserManager class
+    /// </summary>
     public interface IUserManager
     {
+        /// <summary>
+        /// This methods implements the login functionality
+        /// </summary>
+        /// <param name="userData">This parameter represents the entire user data</param>
+        /// <returns>This methods returns the result of the Register functionality in the form of a string</returns>
         string Register(RegisterModel userData);
+
+        /// <summary>
+        /// This method implements the Login functionality
+        /// </summary>
+        /// <param name="login">Login model is passed as a parameter</param>
+        /// <returns>This methods returns the result of the Login functionality in the form of a string</returns>
         string LogIn(LoginModel login);
+
+        /// <summary>
+        /// This method resets the password for the user registration functionality
+        /// </summary>
+        /// <param name="userData">This parameter represents the entire user data</param>
+        /// <returns>This methods returns the result of the Reset Password functionality in the form of a string</returns>
         string ResetPassword(ResetPasswordModel userData);
+        
+        // <summary>
+        /// This method is implements the forget password functionality
+        /// </summary>
+        /// <param name="email">This parameter represents an Email where the new password is to be sent</param>
+        /// <returns>This methods returns the result of the Forget Password functionality in the form of a string</returns>
         string ForgottenPassword(string email);
+
+        /// <summary>
+        /// This method generates the JWT for the user login functionality.
+        /// </summary>
+        /// <param name="email">This parameter represents an email whose JWT is to be generated</param>
+        /// <returns>It returns a JWT in the form of an encrypted string</returns>
         string JWTGenerator(string email);
     }
 }
--- FundooManager/Manager/UserManager.cs
+++ FundooManager/Manager/UserManager.cs
@@ -1,21 +1,44 @@
-﻿using FundooManager.Interface;
-using FundooModel;
-using FundooRepository.Interface;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UserManager.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Mohammad Hanif"/>
+// ----------------------------------------------------------------------------------------------------------
 
 namespace FundooManager.Manager
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+    using FundooManager.Interface;
+    using FundooModel;
+    using FundooRepository.Interface;
+
+    /// <summary>
+    /// This is a class for UserManager
+    /// </summary>
     public class UserManager : IUserManager
     {
+        /// <summary>
+        /// Object created for IUserRepository
+        /// </summary>
         private readonly IUserRepository repository;
+
+        /// <summary>
+        /// Initializes a new instance of the UserManager class
+        /// </summary>
+        /// <param name="repository">The parameter passed is the object of the IUserRepository class</param>
         public UserManager(IUserRepository repository)
         {
             this.repository = repository;
         }
 
+        /// <summary>
+        /// This methods implements the login functionality
+        /// </summary>
+        /// <param name="userData">This parameter represents the entire user data</param>
+        /// <returns>This methods returns the result of the Register functionality in the form of a string</returns>
         public string Register(RegisterModel userData)
         {
             try
@@ -26,8 +49,13 @@ namespace FundooManager.Manager
             {
                 throw new Exception(e.Message);
             }
-
         }
+
+        /// <summary>
+        /// This method implements the Login functionality
+        /// </summary>
+        /// <param name="login">Login model is passed as a parameter</param>
+        /// <returns>This methods returns the result of the Login functionality in the form of a string</returns>
         public string LogIn(LoginModel login)
         {
             try
@@ -39,6 +67,12 @@ namespace FundooManager.Manager
                 throw new Exception(e.Message);
             }
         }
+
+        /// <summary>
+        /// This method resets the password for the user registration functionality
+        /// </summary>
+        /// <param name="userData">This parameter represents the entire user data</param>
+        /// <returns>This methods returns the result of the Reset Password functionality in the form of a string</returns>
         public string ResetPassword(ResetPasswordModel userData)
         {
             try
@@ -50,6 +84,12 @@ namespace FundooManager.Manager
                 throw new Exception(ex.Message);
             }
         }
+
+        /// <summary>
+        /// This method is implements the forget password functionality
+        /// </summary>
+        /// <param name="email">This parameter represents an Email where the new password is to be sent</param>
+        /// <returns>This methods returns the result of the Forget Password functionality in the form of a string</returns>
         public string ForgottenPassword(string email)
         {
             try
@@ -61,6 +101,12 @@ namespace FundooManager.Manager
                 throw new Exception(ex.Message);
             }
         }
+
+        /// <summary>
+        /// This method generates the JWT for the user login functionality.
+        /// </summary>
+        /// <param name="email">This parameter represents an email whose JWT is to be generated</param>
+        /// <returns>It returns a JWT in the form of an encrypted string</returns>
         public string JWTGenerator(string email)
         {
             try
